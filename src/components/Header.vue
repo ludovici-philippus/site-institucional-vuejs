d6759c77b2ebdf163babe5b46a14005911654b98
Luis Felipe <202makertv@gmail.com>
2022-02-20 16:03:24 -0300

    Inicialização da criação e estilização do Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <div class="overlay"></div>
         <div class="header__content">
             <div class="content__logo">
-                <router-link to="/">Empresa</router-link> 
+                <router-link to="/"><h1>Empresa</h1></router-link> 
             </div>
             <nav class="desktop">
 
@@ -19,5 +19,36 @@ name: "Header"
 </script>
 
 <style scoped>
+header{
+    width: 100%;
+    min-height: 50vh;
+    background-image: url("../assets/images/bg.jpg");
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    z-index: 1;
+}
+.overlay{
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    min-height: 50vh;
+}
+
+.header__content{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 3;
+}
+
+.header__content a{
+    text-decoration: none;
+    color: white;
+}
 
 </style>
