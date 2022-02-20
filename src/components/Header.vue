7613325ebc4b52bf99e0cfcfe422da30de983114
Luis Felipe <202makertv@gmail.com>
2022-02-20 15:56:26 -0300

    Primeiro commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <div class="overlay"></div>
+        <div class="header__content">
+            <div class="content__logo">
+                <router-link to="/">Empresa</router-link> 
+            </div>
+            <nav class="desktop">
+
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default{
+name: "Header"
+} 
+</script>
+
+<style scoped>
+
+</style>
